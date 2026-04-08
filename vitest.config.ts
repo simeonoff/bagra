63b722b88d531df561909be62eabfbdff14663b1
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-04-08 17:57:17 +0300

    chore: remove the playground package from both tests and root tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
       'packages/wasm',
       'packages/themes',
       'packages/rehype',
-      'packages/playground',
       {
         plugins: [wasmInlinePlugin()],
         resolve: {
