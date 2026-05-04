f55c423967b0f49fd9eeb5384f6f81e0e8beafd0
simeonoff <4118303+simeonoff@users.noreply.github.com>
2026-05-04 07:06:23 +0000

    chore(themes): update base16 schemes from tinted-theming

---
+++ packages/themes/src/base16/everforest-light-medium.ts
@@ -0,0 +1,26 @@
+import type { BagraTheme } from '@bagrajs/core';
+
+export const everforestLightMedium: BagraTheme = {
+  name: 'everforest-light-medium',
+  displayName: 'Everforest Light (Medium)',
+  variant: 'light',
+  author: 'Márcio Sobel (https://github.com/marciosobel)',
+  colors: {
+    base00: '#fdf6e3',
+    base01: '#f4f0d9',
+    base02: '#e6e2cc',
+    base03: '#939f91',
+    base04: '#829181',
+    base05: '#5c6a72',
+    base06: '#475258',
+    base07: '#2d353b',
+    base08: '#f85552',
+    base09: '#f57d26',
+    base0A: '#dfa000',
+    base0B: '#8da101',
+    base0C: '#35a77c',
+    base0D: '#3a94c5',
+    base0E: '#df69ba',
+    base0F: '#829181',
+  },
+};
---
+++ packages/themes/src/base16/swamp-dark.ts
@@ -0,0 +1,26 @@
+import type { BagraTheme } from '@bagrajs/core';
+
+export const swampDark: BagraTheme = {
+  name: 'swamp-dark',
+  displayName: 'Swamp Dark',
+  variant: 'dark',
+  author: 'Masroof Maindak (https://github.com/masroof-maindak)',
+  colors: {
+    base00: '#242015',
+    base01: '#3a3124',
+    base02: '#4d3f32',
+    base03: '#5f4e41',
+    base04: '#b8a58c',
+    base05: '#d2c3a4',
+    base06: '#ebe0bb',
+    base07: '#f1e9d0',
+    base08: '#db930d',
+    base09: '#ebe0bb',
+    base0A: '#a82d56',
+    base0B: '#7a7653',
+    base0C: '#db930d',
+    base0D: '#c1666b',
+    base0E: '#91506c',
+    base0F: '#61a0a8',
+  },
+};
